46cb77ea031f9f20755d2f1681348c4ba514acb6
lucca713 <luccacerasomma03@gmail.com>
2024-04-10 21:50:54 -0300

    JS e Node js, aprendendo condicionais e listas

---
+++ condicionais.js
@@ -0,0 +1,25 @@
+console.log('trabalhando com condicionais ');
+
+//cria a lista
+const listaDeDestino = new Array(
+
+    'salvador', 'sao paulo', 'rio de janeiro','Bahia'
+);
+
+const idadeComprador = 18;
+
+console.log(listaDeDestino);
+
+if( idadeComprador >= 18){
+    console.log('comprador eh maior de idade');
+    listaDeDestino.splice(1,1);
+
+}else{
+
+    console.log(`comprador nao eh maior de idade nao posso vender ele tem ${idadeComprador}`);
+}
+
+
+
+console.log(listaDeDestino);
+
