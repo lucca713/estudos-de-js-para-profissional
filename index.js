5a9e418663bf7424108ce5923affd32624a43a6e
lucca713 <luccacerasomma03@gmail.com>
2024-07-07 21:27:26 -0300

    colocando classes privadas e mudando o codigo.

--- index.js
+++ index.js
@@ -6,33 +6,42 @@ class Cliente{
 
 class Contacorrente{
     agencia;
-    saldo;
+    
+    #saldo = 0; 
 
     sacar(valor){
-        if(this.saldo >= valor){
+        if(this.#saldo >= valor){
 
-            this.saldo -= valor;
+            this.#saldo -= valor;
         }
 
     }  
+
+    depositar(valor){
+        if(valor > 0){
+            this.#saldo += valor;
+            
+        }
+    }
 }
 
 
-const contacorrenteRicardo = new Contacorrente();
+const contacorrenteRicardo = new Contacorrente();  
 const cliente1 = new Cliente();
 
-contacorrenteRicardo.saldo = 0;
+
 contacorrenteRicardo.agencia = 1001;
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 40354419889;
 
-console.log(contacorrenteRicardo.saldo);
 
-contacorrenteRicardo.saldo = 100;
 
-console.log(contacorrenteRicardo.saldo);
+contacorrenteRicardo.depositar(100);
+contacorrenteRicardo.depositar(100);
+
+
 
 contacorrenteRicardo.sacar(20);
 
-console.log(contacorrenteRicardo.saldo);
+console.log(contacorrenteRicardo);
